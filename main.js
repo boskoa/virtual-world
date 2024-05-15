99da007a6c97a4c1175a857bc476bebe06982898
boskoa <bosko.ap@gmail.com>
2024-05-15 21:08:44 +0200

    Finished mini map

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const carCanvas = document.getElementById("carCanvas");
 carCanvas.width = window.innerWidth - 330;
+carCanvas.height = window.innerHeight;
 const networkCanvas = document.getElementById("networkCanvas");
 networkCanvas.width = 300;
-carCanvas.height = window.innerHeight;
-networkCanvas.height = window.innerHeight;
+networkCanvas.height = window.innerHeight - 300;
+const miniMapCanvas = document.getElementById("miniMapCanvas");
+miniMapCanvas.width = 300;
+miniMapCanvas.height = 300;
 
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
@@ -13,6 +16,7 @@ const worldInfo = worldString ? JSON.parse(worldString) : null;
 const world = worldInfo ? World.load(worldInfo) : new World(new Graph());
  */
 const viewport = new Viewport(carCanvas, world.zoom, world.offset);
+const miniMap = new MiniMap(miniMapCanvas, world.graph, 300);
 
 const N = 1;
 const trafficCount = 20;
@@ -102,6 +106,7 @@ function animate(time) {
   viewport.reset();
   const viewPoint = scale(viewport.getOffset(), -1);
   world.draw(carCtx, viewPoint, false);
+  miniMap.update(viewPoint);
 
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].draw(carCtx);
